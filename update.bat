b000a06b6f195bc0f2f3fa8835ea5f7610b48e50
NCDataDashboard <nathanayoung@msn.com>
2020-02-06 09:36:43 -0500

    updating update.bat

--- update.bat
+++ update.bat
@@ -1,4 +1,16 @@
-git config --global user.name "NCDataDashboard"
+python -m pip install --upgrade pip --user
+pip install jupyter --upgrade --user
+pip install jupyter lab --upgrade --user
+pip install pandas --upgrade --user
+pip install tensorflow --upgrade
+pip install Requests --upgrade
+pip install watermark --upgrade
+pip install xlrd --upgrade
+pip install pyodbc --upgrade
+pip install sqlalchemy --upgrade
+pip install numpy --upgrade
+
+git config --global user.name "nathayoung"
 
 cd Land
 call land.bat
