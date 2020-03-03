7ab797d1623a413b7264150f87fff9717c25bfb4
nathayoung <nathanayoung@msn.com>
2020-03-03 06:54:38 -0500

    Land Update Tue 03/03/2020

--- update.bat
+++ update.bat
@@ -13,29 +13,43 @@ pip install numpy --upgrade
 git config --global user.name "nathayoung"
 git config --global push.default matching
 
+cls
+
 cd Land
 call land.bat
 cd..
+
+cls
 
 cd Labor
 call labor.bat
 cd..
 
+cls
+
 cd Earnings
 call earnings.bat
 cd..
+
+cls
 
 cd Demographics
 call Demographics.bat
 cd..
 
+cls
+
 REM cd Health
 REM start health.bat
 REM cd..
 
+cls
+
 REM cd Natural Products
 REM start natproducts.bat
 REM cd..
+
+cls
 
 git status
 
