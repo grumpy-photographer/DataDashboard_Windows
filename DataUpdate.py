16eabb2cc261b3102a3bcc18a2d53dc1b4c131a0
nathayoung <nathanayoung@msn.com>
2020-03-12 18:10:59 -0400

    fixing DataUpdate.py

--- DataUpdate.py
+++ DataUpdate.py
@@ -621,7 +621,7 @@ try:
             df_nc_join = df_nc.set_index('RegionName').join(df_fips.set_index('RegionName'))
             df_nc_join.loc[ :, 'MunicipalCodeFIPS'] = df_nc_join['MunicipalCodeFIPS'].astype(str)
             df_nc_join.loc[ :, 'MunicipalCodeFIPS'] = df_nc_join['MunicipalCodeFIPS'].str.zfill(3)
-            columns = ['State','Metro','StateCodeFIPS','MunicipalCodeFIPS','SizeRank','2008-03','2008-04','2008-05','2008-06','2008-07','2008-08','2008-09','2008-10','2008-11','2008-12','2009-01','2009-02','2009-03','2009-04','2009-05','2009-06','2009-07','2009-08','2009-09','2009-10','2009-11','2009-12','2010-01','2010-02','2010-03','2010-04','2010-05','2010-06','2010-07','2010-08','2010-09','2010-10','2010-11','2010-12','2011-01','2011-02','2011-03','2011-04','2011-05','2011-06','2011-07','2011-08','2011-09','2011-10','2011-11','2011-12','2012-01','2012-02','2012-03','2012-04','2012-05','2012-06','2012-07','2012-08','2012-09','2012-10','2012-11','2012-12','2013-01','2013-02','2013-03','2013-04','2013-05','2013-06','2013-07','2013-08','2013-09','2013-10','2013-11','2013-12','2014-01','2014-02','2014-03','2014-04','2014-05','2014-06','2014-07','2014-08','2014-09','2014-10','2014-11','2014-12','2015-01','2015-02','2015-03','2015-04','2015-05','2015-06','2015-07','2015-08','2015-09','2015-10','2015-11','2015-12','2016-01','2016-02','2016-03','2016-04','2016-05','2016-06','2016-07','2016-08','2016-09','2016-10','2016-11','2016-12','2017-01','2017-02','2017-03','2017-04','2017-05','2017-06','2017-07','2017-08','2017-09','2017-10','2017-11','2017-12','2018-01','2018-02','2018-03','2018-04','2018-05','2018-06','2018-07','2018-08','2018-09','2018-10','2018-11','2018-12','2019-01','2019-02','2019-03','2019-04','2019-05','2019-06','2019-07','2019-08','2019-09','2019-10','2019-11','2019-12','2020-01','2020-02','2020-03','2020-04','2020-05','2020-06']
+            columns = ['State','Metro','StateCodeFIPS','MunicipalCodeFIPS','SizeRank','2008-03','2008-04','2008-05','2008-06','2008-07','2008-08','2008-09','2008-10','2008-11','2008-12','2009-01','2009-02','2009-03','2009-04','2009-05','2009-06','2009-07','2009-08','2009-09','2009-10','2009-11','2009-12','2010-01','2010-02','2010-03','2010-04','2010-05','2010-06','2010-07','2010-08','2010-09','2010-10','2010-11','2010-12','2011-01','2011-02','2011-03','2011-04','2011-05','2011-06','2011-07','2011-08','2011-09','2011-10','2011-11','2011-12','2012-01','2012-02','2012-03','2012-04','2012-05','2012-06','2012-07','2012-08','2012-09','2012-10','2012-11','2012-12','2013-01','2013-02','2013-03','2013-04','2013-05','2013-06','2013-07','2013-08','2013-09','2013-10','2013-11','2013-12','2014-01','2014-02','2014-03','2014-04','2014-05','2014-06','2014-07','2014-08','2014-09','2014-10','2014-11','2014-12','2015-01','2015-02','2015-03','2015-04','2015-05','2015-06','2015-07','2015-08','2015-09','2015-10','2015-11','2015-12','2016-01','2016-02','2016-03','2016-04','2016-05','2016-06','2016-07','2016-08','2016-09','2016-10','2016-11','2016-12','2017-01','2017-02','2017-03','2017-04','2017-05','2017-06','2017-07','2017-08','2017-09','2017-10','2017-11','2017-12','2018-01','2018-02','2018-03','2018-04','2018-05','2018-06','2018-07','2018-08','2018-09','2018-10','2018-11','2018-12','2019-01','2019-02','2019-03','2019-04','2019-05','2019-06','2019-07','2019-08','2019-09','2019-10','2019-11','2019-12','2020-01']
             df_nc_join = df_nc_join[columns]
             df_nc_join.to_csv(filename, sep ='\t')
             pass
@@ -682,7 +682,6 @@ try:
                     df['GeoFIPS'] = df['GeoFIPS'].replace({"":''})
                     df.set_index(df['GeoFIPS'], inplace = True)
                     df.drop('GeoFIPS', axis = 1, inplace = True)           
-                    print('Updating Labor version.')
                     linecodes = {10:'./Updates/STG_BEA__CA5N_PersonalIncome.txt', 20:'./Updates/STG_BEA_CA5N_Population.txt', 30:'./Updates/STG_BEA_CA5N_Per_Capita_Personal_Income.txt', 35:'./Updates/STG_BEA_CA5N_Earnings_by_Place_of_Work.txt'}
                     for key, value in linecodes.items():
                         filter1 = df['LineCode'] == key
@@ -720,14 +719,23 @@ try:
     # Clean Earnings BEA Data
     def EarningsBEA():
         print('Updating CAINC5N Earnings Version.')
-        linecodes = {10:'./Updates/STG_BEA_CA5N_PersonalIncome.txt', 20:'./Updates/STG_BEA_CA5N_Population.txt', 30:'./Updates/STG_BEA_CA5N_Per_Capita_Personal_Income.txt', 35:'./Updates/STG_BEA_CA5N_Earnings_by_Place_of_Work.txt', 50:'./Updates/STG_BEA_CA5N_Wages_and_Salaries.txt', 60:'./Updates/STG_BEA_CA5N_Supplements_to_Wages_and_Salaries.txt', 70:'./Updates/STG_BEA_CA5N_Proprietors_Income.txt', 81:'./Upates/STG_BEA_CA5N_Farm_Earnings.txt', 82:'./Updates/STG_BEA_CA5N_Nonfarm_Earnings.txt', 90:'./Updates/STG_BEA_CA5N_Private_NonFarm_Earnings.txt', 100:'./Updates/STG_BEA_CA5N_Forestry_Fishing_and_Related_Activities.txt', 200:'./Updates/STG_BEA_CA5N_Mining_Quarrying_and_Oil_and_Technical_Services.txt', 300:'./Updates/STG_BEA_CA5N_Utilities.txt', 400:'./Updates/STG_BEA_CA5N_Construction.txt', 500:'./Updates/STG_BEA_CA5N_Manufacturing.txt', 600:'./Updates/STG_BEA_CA5N_Wholesale_Trade.txt', 700:'./Updates/STG_BEA_CA5N_Retail_Trade.txt', 800:'./Updates/STG_BEA_CA5N_Transporatation_and_Warehousing.txt', 900:'./Updates/STG_BEA_CA5N_Information.txt', 1000:'./Updates/STG_BEA_CA5N_Finance_and_Insurance.txt', 1100:'./Updates/STG_BEA_CA5N_Real_Estate_and_Rental_and_Leasing.txt', 1200:'./Updates/STG_BEA_CA5N_Professional_Scientific_and_Technical_Services.txt', 1300:'./Updates/STG_BEA_CA5N_Management_of_Companies_and_Enterprises.txt', 1400:'./Updates/STG_BEA_CA5N_Administrative_and_Support_and_Waste_Management_and_Remediation_Services.txt', 1500:'./Updates/STG_BEA_CA5N_Educational_Services.txt', 1600:'./Updates/STG_BEA_CA5N_Health_Care_and_Social_Assistance.txt', 1700:'./Updates/STG_BEA_CA5N_Arts_Entertainment_and_Recreation.txt', 1800:'./Updates/STG_BEA_CA5N_Accommodation_and_Food_Services.txt', 1900:'./Updates/STG_BEA_CA5N_Other_Services.txt', 2000:'./Updates/STG_BEA_CA5N_Government_and_Government_Enterprises.txt', 2001:'./Updates/STG_BEA_CA5N_Federal_Civilian.txt', 2002:'./Updates/STG_BEA_CA5N_Military.txt', 2010:'./Updates/STG_BEA_CA5N_State_and_Local.txt', 2011:'./Updates/STG_BEA_CA5N_State_Government.txt', 2012:'./Updates/STG_BEA_CA5N_Local_Government.txt'}
-        for key, value in linecodes.items():
-            filter1 = df['LineCode'] == key
-            df_filtered = df[filter1]
-            df_filtered.to_csv(value, sep = '\t')
-            pass
+        response = requests.get('https://apps.bea.gov/regional/zip/CAINC5N.zip')
+        zip = ZipFile(BytesIO(response.content))
+        files = zip.namelist()
+        with zip.open(files[34]) as csvfile:
+            df = pd.read_csv(csvfile, encoding='ISO-8859-1', sep=",")
+            df.drop(df.tail(4).index,inplace=True)
+            df['GeoFIPS'] = df['GeoFIPS'].replace({"":''})
+            df.set_index(df['GeoFIPS'], inplace = True)
+            df.drop('GeoFIPS', axis = 1, inplace = True)   
+            linecodes = {10:'./Updates/STG_BEA_CA5N_PersonalIncome.txt', 20:'./Updates/STG_BEA_CA5N_Population.txt', 30:'./Updates/STG_BEA_CA5N_Per_Capita_Personal_Income.txt', 35:'./Updates/STG_BEA_CA5N_Earnings_by_Place_of_Work.txt', 50:'./Updates/STG_BEA_CA5N_Wages_and_Salaries.txt', 60:'./Updates/STG_BEA_CA5N_Supplements_to_Wages_and_Salaries.txt', 70:'./Updates/STG_BEA_CA5N_Proprietors_Income.txt', 81:'./Updates/STG_BEA_CA5N_Farm_Earnings.txt', 82:'./Updates/STG_BEA_CA5N_Nonfarm_Earnings.txt', 90:'./Updates/STG_BEA_CA5N_Private_NonFarm_Earnings.txt', 100:'./Updates/STG_BEA_CA5N_Forestry_Fishing_and_Related_Activities.txt', 200:'./Updates/STG_BEA_CA5N_Mining_Quarrying_and_Oil_and_Technical_Services.txt', 300:'./Updates/STG_BEA_CA5N_Utilities.txt', 400:'./Updates/STG_BEA_CA5N_Construction.txt', 500:'./Updates/STG_BEA_CA5N_Manufacturing.txt', 600:'./Updates/STG_BEA_CA5N_Wholesale_Trade.txt', 700:'./Updates/STG_BEA_CA5N_Retail_Trade.txt', 800:'./Updates/STG_BEA_CA5N_Transporatation_and_Warehousing.txt', 900:'./Updates/STG_BEA_CA5N_Information.txt', 1000:'./Updates/STG_BEA_CA5N_Finance_and_Insurance.txt', 1100:'./Updates/STG_BEA_CA5N_Real_Estate_and_Rental_and_Leasing.txt', 1200:'./Updates/STG_BEA_CA5N_Professional_Scientific_and_Technical_Services.txt', 1300:'./Updates/STG_BEA_CA5N_Management_of_Companies_and_Enterprises.txt', 1400:'./Updates/STG_BEA_CA5N_Administrative_and_Support_and_Waste_Management_and_Remediation_Services.txt', 1500:'./Updates/STG_BEA_CA5N_Educational_Services.txt', 1600:'./Updates/STG_BEA_CA5N_Health_Care_and_Social_Assistance.txt', 1700:'./Updates/STG_BEA_CA5N_Arts_Entertainment_and_Recreation.txt', 1800:'./Updates/STG_BEA_CA5N_Accommodation_and_Food_Services.txt', 1900:'./Updates/STG_BEA_CA5N_Other_Services.txt', 2000:'./Updates/STG_BEA_CA5N_Government_and_Government_Enterprises.txt', 2001:'./Updates/STG_BEA_CA5N_Federal_Civilian.txt', 2002:'./Updates/STG_BEA_CA5N_Military.txt', 2010:'./Updates/STG_BEA_CA5N_State_and_Local.txt', 2011:'./Updates/STG_BEA_CA5N_State_Government.txt', 2012:'./Updates/STG_BEA_CA5N_Local_Government.txt'}
+            for key, value in linecodes.items():
+                filter1 = df['LineCode'] == key
+                df_filtered = df[filter1]
+                df_filtered.to_csv(value, sep = '\t')
+                pass
         while True:
-            EarningsBEA()
+            endProgram()
 
     #Clean NC Tax data
     def NCDOR(): #done
@@ -816,7 +824,7 @@ try:
             if source == 1:
                 FRED()
             elif source == 2:
-                laborBEA()
+                LaborBEA()
         else:
             print('Please enter 1 for GeoFred or 2 for BEA.')
             labor_update()
@@ -833,8 +841,18 @@ try:
             elif source == 2:
                 FRED()
         else:
-            print('Please enter 1 for ZLLW or 2 for GeoFRED.')
-            pass
+            answer = int(input('Would you like to update this folder again? Type 0 for options. '))
+            if answer == 1:
+                print('Restarting Land')
+                land_update()
+                pass
+            elif answer == 2:
+                endProgram()
+            elif answer == 0:
+                print('1-Yes\n2-No')
+                pass
+            else:
+                print('Please enter a numeric value.')
         while True:
             endProgram()
 
@@ -845,7 +863,7 @@ try:
         column_list = df.columns.values
         for i in column_list:
             df.loc[df[i].isnull(),i]=''
-        df.to_csv('./Updates/STG_Natural_Products.txt', sep='\t')
+            df.to_csv('./Updates/STG_Natural_Products.txt', sep='\t')
         while True:
             endProgram()
 
