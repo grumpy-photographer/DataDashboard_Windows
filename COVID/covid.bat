4620b345cc90c7e45d66f3383c03a77a931c3dc4
nathayoung <nathanayoung@msn.com>
2020-04-12 13:39:11 -0400

    adding COVID

---
+++ COVID/covid.bat
@@ -0,0 +1,13 @@
+Python /Scripts/NC_Cases_by_Congregate_Living.py
+Python /Scripts/NC_Cases_by_County.py
+Python /Scripts/NC_Cases/by_Race.py
+
+cd Updates
+git status
+
+git commit -a -m "COVID Update %date%"
+git status
+
+git push
+
+cd..
