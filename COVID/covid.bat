be7d44d65fc62132b09e66fc32e435ee7189978e
nathayoung <nathanayoung@msn.com>
2020-04-12 13:43:41 -0400

    bug fixing

--- COVID/covid.bat
+++ COVID/covid.bat
@@ -1,6 +1,6 @@
-Python /Scripts/NC_Cases_by_Congregate_Living.py
-Python /Scripts/NC_Cases_by_County.py
-Python /Scripts/NC_Cases/by_Race.py
+Python Scripts\NC_Cases_by_Congregate_Living.py
+Python Scripts\NC_Cases_by_County.py
+Python Scripts\NC_Cases_by_Race.py
 
 cd Updates
 git status
