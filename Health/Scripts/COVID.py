02d73b09f53b4e7c5be807e4b2d9aa0b6dff3f00
nathayoung <nathanayoung@msn.com>
2020-04-22 11:02:25 -0400

    COVID Update Wed 04/22/2020

--- Health/Scripts/COVID.py
+++ Health/Scripts/COVID.py
@@ -12,6 +12,6 @@ df = df.drop(['deaths', 'state'], axis=1)
 
 df.set_index('GeoArea_FIPS', inplace =True)
 
-df = df.columns('GeoArea_FIPS', 'GeoArea_Name', 'Data_Period_Business_Key', 'Cases')
+df = df.columns('GeoArea_Name', 'Data_Period_Business_Key', 'Cases')
 
 df.to_csv('./Updates/covid_data.txt', sep='\t')
