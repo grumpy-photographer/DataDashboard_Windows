d17eb0e89d63f680391f87128e1374ce7f61a2b8
nathayoung <nathanayoung@msn.com>
2020-04-22 12:20:49 -0400

    Adding COVID

---
+++ Health/Scripts/COVID_Cases.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import subprocess
+
+#create backups
+df_backup = pd.read_csv('./Updates/STG_NYTI_CNTY_COVID_19_Cases.txt', sep='\t')
+df_backup.to_csv('./Backups/STG_NYTI_CNTY_COVID_19_Cases_BACKUP.txt', sep='\t')
+
+#read data
+df = pd.read_csv('./Data/covid-19-data/us-counties.csv')
+
+#fitler to nc
+filter1 = df['state'].str.contains('North Carolina')
+df = df[filter1]
+
+#clean
+df['fips'] = df['fips'].astype(int)
+df = df.rename(columns = {"fips":'GeoArea_FIPS', 'county':'GeoArea_Name', 'cases':'Estimated_Value', 'date':'Data_Period_Business_Key'})
+df = df.drop(['deaths', 'state'], axis=1)
+
+#add missing columns to match database
+df['Economic_Measure_Code'] = 'NYTI_CNTY_COV01'
+df['Economic_Measure_Name'] = 'COVID-19 Confirmed Cases'
+df['Measure_Name'] = ''
+df['Unit_of_Measure_Code'] = 'Count'
+
+#reset columns
+columns = ['GeoArea_FIPS', 'GeoArea_Name', 'Economic_Measure_Code', 'Economic_Measure_Name', 'Measure_Name', 'Data_Period_Business_Key', 'Estimated_Value']
+df = df[columns]
+df.set_index('GeoArea_FIPS', inplace =True)
+
+
+
+#save as txt
+df.to_csv('./Updates/STG_NYTI_CNTY_COVID_19_Cases.txt', sep='\t')
---
+++ Health/Scripts/COVID_Deaths.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import subprocess
+
+#create backups
+df_backup = pd.read_csv('./Updates/STG_NYTI_CNTY_COVID_19_Deaths.txt', sep='\t')
+df_backup.to_csv('./Backups/STG_NYTI_CNTY_COVID_19_Deaths_BACKUP.txt', sep='\t')
+
+#read data
+df = pd.read_csv('./Data/covid-19-data/us-counties.csv')
+
+#fitler to nc
+filter1 = df['state'].str.contains('North Carolina')
+df = df[filter1]
+
+#clean
+df['fips'] = df['fips'].astype(int)
+df = df.rename(columns = {"fips":'GeoArea_FIPS', 'county':'GeoArea_Name', 'deaths':'Estimated_Value', 'date':'Data_Period_Business_Key'})
+df = df.drop(['cases', 'state'], axis=1)
+
+#add missing columns to match database
+df['Economic_Measure_Code'] = 'NYTI_CNTY_COV02'
+df['Economic_Measure_Name'] = 'COVID-19 Confirmed Deaths'
+df['Measure_Name'] = ''
+df['Unit_of_Measure_Code'] = 'Count'
+
+#reset columns
+columns = ['GeoArea_FIPS', 'GeoArea_Name', 'Economic_Measure_Code', 'Economic_Measure_Name', 'Measure_Name', 'Data_Period_Business_Key', 'Estimated_Value']
+df = df[columns]
+df.set_index('GeoArea_FIPS', inplace =True)
+
+#save as txt
+df.to_csv('./Updates/STG_NYTI_CNTY_COVID_19_Deaths.txt', sep='\t')
