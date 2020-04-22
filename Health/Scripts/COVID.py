c3d1d3a88b1cb32127d75e4cc05145933d913136
nathayoung <nathanayoung@msn.com>
2020-04-22 11:02:05 -0400

    COVID Update Wed 04/22/2020

--- Health/Scripts/COVID.py
+++ Health/Scripts/COVID.py
@@ -10,7 +10,7 @@ df['fips'] = df['fips'].astype(int)
 df = df.rename(columns = {"fips":'GeoArea_FIPS', 'county':'GeoArea_Name', 'cases':'Cases', 'date':'Data_Period_Business_Key'})
 df = df.drop(['deaths', 'state'], axis=1)
 
-df = df.set_index('GeoArea_FIPS', inplace =True)
+df.set_index('GeoArea_FIPS', inplace =True)
 
 df = df.columns('GeoArea_FIPS', 'GeoArea_Name', 'Data_Period_Business_Key', 'Cases')
 
