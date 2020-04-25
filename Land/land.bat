2d1feac2e6afe6cc13d9f534a0d1025339234a45
nathayoung <nathanayoung@msn.com>
2020-04-25 01:55:24 -0400

    adding Publish_Land_Data_Series

--- Land/land.bat
+++ Land/land.bat
@@ -6,6 +6,7 @@ python -W ignore Scripts\County_Zhvi_AllHomes.py
 python -W ignore Scripts\GeoFRED_All_Transactions_House_Price_Index.py
 python -W ignore Scripts\GeoFRED_Homeownership_Rate_by_County.py
 python -W ignore Scripts\GeoFRED_New_Private_Housing_Structures.py
+python -W ignore Scripts\Publish_Land_Data_Series.py
 
 cd Updates
 git status
