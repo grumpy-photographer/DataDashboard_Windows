053513f9809943537228cf811931bc1485a6aa92
nathayoung <nathanayoung@msn.com>
2020-05-01 11:45:02 -0400

    Earnings Update Fri 05/01/2020

--- Earnings/Scripts/CAINC5N_NC.py
+++ Earnings/Scripts/CAINC5N_NC.py
@@ -1808,12 +1808,12 @@ for i in column_list:
     df_private.loc[df_private[i].isnull(), i] = 0
 
 # Drop old backup table
-c.execute("drop table STG_BEA_CA5N_Private_Nonfarm_Earnings_BACKUP")
+#c.execute("drop table STG_BEA_CA5N_Private_Nonfarm_Earnings_BACKUP")
 
 # Create new backup
-c.execute(
-    """sp_rename 'dbo.STG_BEA_CA5N_Private_Nonfarm_Earnings','STG_BEA_CA5N_Private_Nonfarm_Earnings_BACKUP';"""
-)
+#c.execute(
+#    """sp_rename 'dbo.STG_BEA_CA5N_Private_Nonfarm_Earnings','STG_BEA_CA5N_Private_Nonfarm_Earnings_BACKUP';"""
+#)
 
 # Create Private_Nonfarm_Earnings Table
 c.execute(
@@ -2015,12 +2015,12 @@ for i in column_list:
     df_nonfarm.loc[df_nonfarm[i].isnull(), i] = 0
 
 # Drop old backup table
-#c.execute("drop table STG_BEA_CA5N_Nonfarm_Earnings_BACKUP")
+c.execute("drop table STG_BEA_CA5N_Nonfarm_Earnings_BACKUP")
 
 # Create new backup
-#c.execute(
-#    """sp_rename 'dbo.STG_BEA_CA5N_Nonfarm_Earnings','STG_BEA_CA5N_Nonfarm_Earnings_BACKUP';"""
-#)
+c.execute(
+    """sp_rename 'dbo.STG_BEA_CA5N_Nonfarm_Earnings','STG_BEA_CA5N_Nonfarm_Earnings_BACKUP';"""
+)
 
 # Create Nonfarm_Earnings Table
 c.execute(
