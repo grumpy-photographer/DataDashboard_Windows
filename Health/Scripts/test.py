fc0f78c232f3dda8b74c266e083ba2d50154fc4c
nathayoung <nathanayoung@msn.com>
2020-08-10 11:22:00 -0400

    Land Update Mon 08/10/2020

--- Health/Scripts/test.py
+++ Health/Scripts/test.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
-df = pd.read_excel('../Data/NC_COVID-19/4.24.2020/ByRace&Ethnicity.xlsx')
-print(df.head())
+df_race = pd.read_excel('../Data/NC_COVID-19/4.24.2020/ByRace&Ethnicity.xlsx')
+print(df_race.head())
+
+df_cls = pd.read_excel('../Data/NC_COVID-19/4.24.2020/ConfirmedCases&Deaths in CLS.xlsx')
+print('\n', df_cls.head())
+
+df_outbreaks = pd.read_excel('../Data/NC_COVID-19/4.24.2020/Ongoing Outbreaks in CLS.xlsx')
+print('\n', df_outbreaks.head())
