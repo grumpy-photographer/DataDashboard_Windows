b2b37e32b875d2b3e8c488bc4846e6de0d087d30
nathayoung <nathanayoung@msn.com>
2020-08-13 11:21:18 -0400

    Land Update Thu 08/13/2020

--- Health/Scripts/test.py
+++ Health/Scripts/test.py
@@ -3,8 +3,8 @@ import pandas as pd
 df_race = pd.read_excel('../Data/NC_COVID-19/4.24.2020/ByRace&Ethnicity.xlsx')
 print(df_race.head())
 
-df_cls = pd.read_excel('../Data/NC_COVID-19/4.24.2020/ConfirmedCases&Deaths in CLS.xlsx')
-print('\n', df_cls.head())
+import glob
 
-df_outbreaks = pd.read_excel('../Data/NC_COVID-19/4.24.2020/Ongoing Outbreaks in CLS.xlsx')
-print('\n', df_outbreaks.head())
+l = [pd.read_excel(filename) for filename in glob.glob("../Data/NC_COVID-19/*/*.excel")]
+df = pd.concat(1, axis=0)
+print(df)
