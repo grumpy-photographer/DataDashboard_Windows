100a39cbe27a674129215e88673da1f9aed4f956
nathayoung <nathanayoung@msn.com>
2020-11-08 11:31:35 -0500

    updates

--- plots.py
+++ plots.py
@@ -1,7 +1,6 @@
-import os
+import glob
 import pandas as pd
 import plotly.express as px
-
 
 def plot_scripts(data_folder):
     """
@@ -13,8 +12,27 @@ def plot_scripts(data_folder):
     -------
     graphs of data from data_folder
     """
-    for x in os.listdir(data_folder):
-        df = pd.read_csv(x)
+    for x in glob.glob(data_folder):
+        if "Data_Series" in x:
+            df = pd.read_csv(x, sep="\t", low_memory=False)
 
-        #px.bar(data_frame=df, x="Data_Period_Business_Key", y="Estimated_Value")
-        print(df)
+            melt_df = df.melt(
+                id_vars=[
+                    "GeoArea_FIPS",
+                    "GeoArea_Name",
+                    "Economic_Measure_Code",
+                    "Economic_Measure_Name",
+                    "Published_UOM",
+                    "Calculation_Type",
+                    "Default_Scale",
+                ],
+                var_name="Date",
+                value_name="Published_Value",
+            )
+            
+            df["GeoArea_FIPS"] = df["GeoArea_FIPS"].astype(str)
+            df["Published_Value"] = df["Published_Value"].astype(float)
+
+    fig = px.scatter(melt_df, x="GeoArea_FIPS", y="Published_Value", color="GeoArea_Name")
+    
+    return fig
