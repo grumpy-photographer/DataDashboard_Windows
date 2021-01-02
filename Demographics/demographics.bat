e114169c7dfd9c5ba8ec4f5005ca2a8be93410e0
nathayoung <nathanayoung@msn.com>
2021-01-02 10:07:40 -0500

    updates

--- Demographics/demographics.bat
+++ Demographics/demographics.bat
@@ -1,7 +1,7 @@
 py -W ignore Scripts\fred_civilian_labor_force.py
 py -W ignore Scripts\fred_eqfx.py
 py -W ignore Scripts\fred_higher_education.py
-py -W ignore Scripts\resident_population.py
+py -W ignore Scripts\fred_resident_population.py
 
 git add *
 git commit -a -m "Demographics Update %Date%"
