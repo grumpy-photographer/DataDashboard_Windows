b886a1fd49cecbba44d1be52edf6ccf052f59fab
nathayoung <nathanayoung@msn.com>
2021-01-02 17:14:24 -0500

    Demographics Update Sat 01/02/2021

--- Demographics/Scripts/fred_data.py
+++ Demographics/Scripts/fred_data.py
@@ -4,16 +4,19 @@ import pandas as pd
 df = pd.DataFrame()
 
 # read civilian labor force data
-df2 = pd.read_csv("../Updates/FRED_Civilian_Labor_Force_by_County_Persons.txt", sep="\t")
+df2 = pd.read_csv(
+    "./Updates/FRED_Civilian_Labor_Force_by_County_Persons.txt", sep="\t")
 
 # read eqfxsubprime data
-df3 = pd.read_csv("../Updates/FRED_EQFXSUBPRIME.txt", sep="\t")
+df3 = pd.read_csv("./Updates/FRED_EQFXSUBPRIME.txt", sep="\t")
 
 # read education data
-df4 = pd.read_csv("../Updates/FRED_People_25_Years_and_Over_Who_Have_Completed_an_Associates_Degree_or_Higher_5year_estimate_by_County_Percent.txt", sep="\t")
+df4 = pd.read_csv(
+    "./Updates/FRED_People_25_Years_and_Over_Who_Have_Completed_an_Associates_Degree_or_Higher_5year_estimate_by_County_Percent.txt", sep="\t")
 
 # read pop data
-df5 = pd.read_csv("../Updates/FRED_Resident_Population_by_County_Thousands_of_Persons.txt", sep="\t")
+df5 = pd.read_csv(
+    "./Updates/FRED_Resident_Population_by_County_Thousands_of_Persons.txt", sep="\t")
 
 # append data
 df_list = [df2, df3, df4, df5]
@@ -25,4 +28,4 @@ data["Region Code"] = data["Region Code"].str.zfill(5)
 data = data.set_index("Region Code")
 
 # save data to text file
-data.to_csv("../Updates/demographics.txt", sep="\t")
+data.to_csv("./Updates/demographics.txt", sep="\t")
