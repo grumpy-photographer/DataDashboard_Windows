6dcc1a74a8082e1c3a646f852cda07bfcc96fc8a
nathayoung <nathanayoung@msn.com>
2021-01-02 11:07:53 -0500

    Demographics Update Sat 01/02/2021

--- Demographics/Scripts/fred_civilian_labor_force.py
+++ Demographics/Scripts/fred_civilian_labor_force.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 # Getting and reading new data
 df = pd.read_excel(
-    "https://geofred.stlouisfed.org/api/download.php?theme=pubugn&colorCount=5&reverseColors=false&intervalMethod=fractile&displayStateOutline=true&lng=-90&lat=40&zoom=4&showLabels=true&showValues=true&regionType=county&seriesTypeId=656&attributes=Not+Seasonally+Adjusted%2C+Monthly%2C+Persons%2C+no_period_desc&aggregationFrequency=Monthly&aggregationType=Average&transformation=lin&date=2030-01-01&type=xls&startDate=1990-01-01&endDate=2030-01-01&mapWidth=2000&mapHeight=1262&hideLegend=false",
+    "https://geofred.stlouisfed.org/api/download.php?theme=pubugn&colorCount=5&reverseColors=false&intervalMethod=fractile&displayStateOutline=true&lng=-90&lat=40&zoom=4&showLabels=true&showValues=true&regionType=county&seriesTypeId=656&attributes=Not+Seasonally+Adjusted%2C+Annual%2C+Persons%2C+no_period_desc&aggregationFrequency=Annual&aggregationType=Average&transformation=lin&date=2030-01-01&type=xls&startDate=1990-01-01&endDate=2030-01-01&mapWidth=1128&mapHeight=1063&hideLegend=false",
     skiprows=1,
 )
 
