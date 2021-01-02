6068f994789bc321fca88a2df584d5501821be22
nathayoung <nathanayoung@msn.com>
2021-01-02 18:09:03 -0500

    Demographics Update Sat 01/02/2021

--- Demographics/Scripts/fred_eqfx.py
+++ Demographics/Scripts/fred_eqfx.py
@@ -13,7 +13,7 @@ df["Region Code"] = df["Region Code"].str.zfill(5)
 df.set_index("Region Code", inplace=True)
 
 # create measure column
-df["Measure_Name"] = "Equixfax Subprime"
+df["Measure_Name"] = "Equifax Subprime Credit Population"
 
 # Drop Series ID column
 df.drop("Series ID", axis=1, inplace=True)
