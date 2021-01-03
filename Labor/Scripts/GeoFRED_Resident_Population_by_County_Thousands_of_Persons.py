6d732f6827db3532626674ee687db51c5192538c
nathayoung <nathanayoung@msn.com>
2021-01-03 12:10:57 -0500

    GDP Update Sun 01/03/2021

--- Labor/Scripts/GeoFRED_Resident_Population_by_County_Thousands_of_Persons.py
+++ Labor/Scripts/GeoFRED_Resident_Population_by_County_Thousands_of_Persons.py
@@ -1,14 +1,6 @@
 # Imports
 import pandas as pd
 
-# Create backups
-df_backup = pd.read_csv(
-    "./Updates/FRED_Resident_Population_by_County_Thousands_of_Persons.txt"
-)
-df_backup.to_csv(
-    "./Backups/FRED_Resident_Population_by_County_Thousands_of_Persons_Thoudands_of_Persons_BACKUP.txt"
-)
-
 # Getting and reading new data
 df = pd.read_excel(
     "https://geofred.stlouisfed.org/api/download.php?theme=pubugn&colorCount=5&reverseColors=false&intervalMethod=fractile&displayStateOutline=true&lng=-90.00&lat=40.01&zoom=4&showLabels=true&showValues=true&regionType=county&seriesTypeId=1549&attributes=Not+Seasonally+Adjusted%2C+Annual%2C+Thousands+of+Persons%2C+no_period_desc&aggregationFrequency=Annual&aggregationType=Average&transformation=lin&date=2030-01-01&type=xls&startDate=1970-01-01&endDate=2030-01-01&mapWidth=2000&mapHeight=1214&hideLegend=false",
--- Labor/Scripts/GeoFRED_Unemployment_Rate_by_County_Percent.py
+++ Labor/Scripts/GeoFRED_Unemployment_Rate_by_County_Percent.py
@@ -1,12 +1,6 @@
 # Imports
 import pandas as pd
 
-# Create backups
-df_backup = pd.read_csv(
-    "./Updates/FRED_Unemployment_Rate_by_County_Percent.txt")
-df_backup.to_csv(
-    "./Backups/FRED_Unemployment_Rate_by_County_Percent_BACKUP.txt")
-
 # Getting and reading new data
 df = pd.read_excel(
     "https://geofred.stlouisfed.org/api/download.php?theme=pubugn&colorCount=5&reverseColors=false&intervalMethod=fractile&displayStateOutline=true&lng=-90.00&lat=40.01&zoom=4&showLabels=true&showValues=true&regionType=county&seriesTypeId=1224&attributes=Not+Seasonally+Adjusted%2C+Monthly%2C+Percent%2C+no_period_desc&aggregationFrequency=Monthly&aggregationType=Average&transformation=lin&date=2030-01-01&type=xls&startDate=1976-01-01&endDate=2030-01-01&mapWidth=2000&mapHeight=1214&hideLegend=false",
