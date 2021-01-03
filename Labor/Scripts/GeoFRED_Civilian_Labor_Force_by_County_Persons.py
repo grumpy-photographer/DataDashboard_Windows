f2a74b15baea6b6eb96b533ba86caf330994bf1b
nathayoung <nathanayoung@msn.com>
2021-01-03 12:10:04 -0500

    COVID Update Sun 01/03/2021-12:09:57.02

--- Labor/Scripts/GeoFRED_Civilian_Labor_Force_by_County_Persons.py
+++ Labor/Scripts/GeoFRED_Civilian_Labor_Force_by_County_Persons.py
@@ -1,18 +1,11 @@
 # Imports
 import pandas as pd
 
-
-# Create backups
-df_backup = pd.read_csv("./Updates/FRED_Civilian_Labor_Force_by_County_Persons.txt")
-df_backup.to_csv("./Backups/FRED_Civilian_Labor_Force_by_County_Persons_BACKUP.txt")
-
-
 # Getting and reading new data
 df = pd.read_excel(
     "https://geofred.stlouisfed.org/api/download.php?theme=pubugn&colorCount=5&reverseColors=false&intervalMethod=fractile&displayStateOutline=true&lng=-90.00&lat=39.98&zoom=4&showLabels=true&showValues=true&regionType=county&seriesTypeId=656&attributes=Not+Seasonally+Adjusted%2C+Monthly%2C+Persons%2C+no_period_desc&aggregationFrequency=Monthly&aggregationType=Average&transformation=lin&date=2030-01-01&type=xls&startDate=1990-01-01&endDate=2030-01-01&mapWidth=2000&mapHeight=1262&hideLegend=false",
     skiprows=1,
 )
-
 
 # Set Series ID as index
 df.set_index(df["Series ID"], inplace=True)
--- Labor/Scripts/GeoFRED_People_25_Years_and_Over_Who_Have_Completed_an_Associates_Degree_or_Higher_5year_estimate_by_County_Percent.py
+++ Labor/Scripts/GeoFRED_People_25_Years_and_Over_Who_Have_Completed_an_Associates_Degree_or_Higher_5year_estimate_by_County_Percent.py
@@ -1,14 +1,6 @@
 # Imports
 import pandas as pd
 
-# Create backups
-df_backup = pd.read_csv(
-    "./Updates/FRED_People_25_Years_and_Over_Who_Have_Completed_an_Associates_Degree_or_Higher_5year_estimate_by_County_Percent.txt"
-)
-df_backup.to_csv(
-    "./Backups/FRED_People_25_Years_and_Over_Who_Have_Completed_an_Associates_Degree_or_Higher_5year_estimate_by_County_Percent_BACKUP.txt"
-)
-
 # Getting and reading new data
 df = pd.read_excel(
     "https://geofred.stlouisfed.org/api/download.php?theme=pubugn&colorCount=5&reverseColors=false&intervalMethod=fractile&displayStateOutline=true&lng=-90.00&lat=40.01&zoom=4&showLabels=true&showValues=true&regionType=county&seriesTypeId=147063&attributes=Not+Seasonally+Adjusted%2C+Annual%2C+Percent%2C+no_period_desc&aggregationFrequency=Annual&aggregationType=Average&transformation=lin&date=2030-01-01&type=xls&startDate=2009-01-01&endDate=2030-01-01&mapWidth=2000&mapHeight=1214&hideLegend=false",
