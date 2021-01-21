b2aad533abdb37164df1f57b7a19308350a2c345
nathayoung <nathanayoung@msn.com>
2021-01-20 22:17:32 -0500

    Labor Update Wed 01/20/2021

--- Labor/labor.bat
+++ Labor/labor.bat
@@ -2,10 +2,10 @@ py -W ignore Scripts\CAINC5N_NC.py
 py -W ignore Scripts\CAINC6N_NC.py
 py -W ignore Scripts\CAINC5N_OH.py
 py -W ignore Scripts\CAINC6N_OH.py
-py -W ignore Scripts\GeoFRED_Civilian_Labor_Force_by_County_Persons.py
-py -W ignore Scripts\GeoFRED_People_25_Years_and_Over_Who_Have_Completed_an_Associates_Degree_or_Higher_5year_estimate_by_County_Percent.py
-py -W ignore Scripts\GeoFRED_Resident_Population_by_County_Thousands_of_Persons.py
-py -W ignore Scripts\GeoFRED_Unemployment_Rate_by_County_Percent.py
+py -W ignore Scripts\fred_civilian_labor_force.py
+py -W ignore Scripts\fred_higher_education.py
+py -W ignore Scripts\fred_resident_population.py
+py -W ignore Scripts\fred_unemployment.py
 
 cd Updates
 git status
