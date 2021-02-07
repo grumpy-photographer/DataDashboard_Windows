c35bbe623440ff4d04ee4b2d2a5e49b3c40b8069
nathayoung <nathanayoung@msn.com>
2021-02-07 09:36:35 -0500

    Land Update Sun 02/07/2021

--- Land/Scripts/fred_homeownership.py
+++ Land/Scripts/fred_homeownership.py
@@ -21,7 +21,9 @@ df.drop("Series ID", axis=1, inplace=True)
 df = df.melt(id_vars=["Region Code", "Region Name", "Measure Name"],
              value_name="Estimated Value", var_name="Date")
 df.set_index("Region Code", inplace=True)
-df["Date"] = pd.to_datetime
+
+# Set Date column to proper dtype
+df["Date"] = pd.to_datetime(df["Date"])
 df["Date"] = df["Date"].dt.date
 
 
