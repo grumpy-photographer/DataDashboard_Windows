89a2317e703d5b20a4e91606f6eec1c7972af04f
nathayoung <nathanayoung@msn.com>
2021-02-14 09:08:12 -0500

    updates

--- Demographics/demographics.bat
+++
@@ -1,9 +0,0 @@
-py -W ignore Scripts\fred_civilian_labor_force.py
-py -W ignore Scripts\fred_eqfx.py
-py -W ignore Scripts\fred_higher_education.py
-py -W ignore Scripts\fred_resident_population.py
-
-git add *
-git commit -a -m "Demographics Update %Date%"
-
-git push
--- Earnings/earnings.bat
+++
@@ -1,7 +0,0 @@
-python -W ignore Scripts/cainc5n.py
-
-git add *
-git commit -a -m "Earnings Update %Date%"
-git status
-
-git push
