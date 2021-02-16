badd13667bcf763a65469ed4d601938f7c8850a7
nathayoung <nathanayoung@msn.com>
2021-02-15 19:25:31 -0500

    Land Update Mon 02/15/2021

---
+++ Land/land.bat
@@ -0,0 +1,12 @@
+python -W ignore Scripts\zllw_zhvi_top.py
+python -W ignore Scripts\zllw_zhvi_bottom.py
+python -W ignore Scripts\fred_house_transactions.py
+python -W ignore Scripts\fred_homeownership.py
+python -W ignore Scripts\fred_private_housing.py
+
+git status
+
+git add *
+
+git commit -a -m "Land Update %date%"
+git push
