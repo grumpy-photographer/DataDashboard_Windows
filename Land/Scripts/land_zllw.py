66adb5b3cb46ec9827c9ecd0f72ad569d98475a1
nathayoung <nathanayoung@msn.com>
2021-02-16 07:11:47 -0500

    Land Update Tue 02/16/2021

---
+++ Land/Scripts/land_zllw.py
@@ -0,0 +1,8 @@
+from DataDashboard_Windows.Modules.zillow import housing
+
+dictionary = {
+    "All Homes Top Tier Time Series": "http://files.zillowstatic.com/research/public_v2/zhvi/County_zhvi_uc_sfrcondo_tier_0.67_1.0_sm_sa_mon.csv",
+    "All Homes Bottom Tier Time Series": "http://files.zillowstatic.com/research/public_v2/zhvi/County_zhvi_uc_sfrcondo_tier_0.0_0.33_sm_sa_mon.csv",
+}
+
+housing(file_dict=dictionary)
