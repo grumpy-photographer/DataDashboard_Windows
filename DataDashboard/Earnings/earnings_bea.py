5c9f8f920b2c577a62b448dd00f47a0a065d3925
nathayoung <nathanayoung@msn.com>
2021-02-16 20:13:45 -0500

    updates

--- DataDashboard/Earnings/earnings_bea.py
+++
@@ -1,5 +0,0 @@
-from DataDashboard_Windows.Modules.bea import unzip
-
-dictionary = {"CAINC5N": "https://apps.bea.gov/regional/zip/CAINC5N.zip"}
-
-unzip(file_dict=dictionary)
--- DataDashboard/Land/land_zllw.py
+++
@@ -1,8 +0,0 @@
-from Modules.zillow import housing
-
-dictionary = {
-    "All Homes Top Tier Time Series": "http://files.zillowstatic.com/research/public_v2/zhvi/County_zhvi_uc_sfrcondo_tier_0.67_1.0_sm_sa_mon.csv",
-    "All Homes Bottom Tier Time Series": "http://files.zillowstatic.com/research/public_v2/zhvi/County_zhvi_uc_sfrcondo_tier_0.0_0.33_sm_sa_mon.csv",
-}
-
-housing(file_dict=dictionary)
